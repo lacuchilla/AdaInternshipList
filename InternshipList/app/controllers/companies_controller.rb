4ed0406cc68d93c3f2c5aae3da6994349e1e4ca7
Amy Hunter <amyrhunter@gmail.com>
2015-11-21 02:29:36 -0800

    Add routes and view page for index for Company model

--- InternshipList/app/controllers/companies_controller.rb
+++ InternshipList/app/controllers/companies_controller.rb
@@ -1,5 +1,32 @@
 class CompaniesController < ApplicationController
   def index
-    @companies = Companies.all
+    @companies = Company.all
+  end
+
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.create(company_params[:company])
+    redirect_to "/companies"
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+private
+
+  def company_params
+    params.permit(company: [:name, :location, :description, :website])
   end
 end
