5864e693346f55ee340cd7233e91503ccce24c2a
Amy Hunter <amyrhunter@gmail.com>
2015-11-21 05:36:51 -0800

    Add better errors gem and work on show page for the people of a compnay, not working quite yet.

--- InternshipList/app/controllers/people_controller.rb
+++ InternshipList/app/controllers/people_controller.rb
@@ -1,3 +1,36 @@
 class PeopleController < ApplicationController
-  belongs_to :company
+  def index
+    @people = Person.all
+  end
+
+  def new
+    @url = "create"
+    @legend = "Add a Contact"
+    @person = Person.new
+  end
+
+  def create
+    Person.create(person_params[:person])
+    redirect_to "/companies/#{@company.id}/people"
+  end
+
+  def show
+    @person = Person.find(params[:id])
+    @company = Company.find(@person.company_id)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def person_params
+    params.permit(task: [:name, :title, :email, :company_id])
+  end
 end
