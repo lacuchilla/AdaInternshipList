f700f5203cdad6d638ba80c65787ba10fa439991
Amy Hunter <amyrhunter@gmail.com>
2015-11-21 10:52:13 -0800

    So much broken code. URL helper paths and params are not working correctly

--- InternshipList/app/controllers/people_controller.rb
+++ InternshipList/app/controllers/people_controller.rb
@@ -1,6 +1,7 @@
 class PeopleController < ApplicationController
   def index
     @company_id = params[:company_id]
+    @company = Company.find(@person.company_id(params[:id]))
     @people = Person.where(company_id: params[:company_id])
   end
 
@@ -21,12 +22,17 @@ class PeopleController < ApplicationController
   end
 
   def edit
+    @url = "edit"
+    @legend = "Update a Contact"
+    @person = person_params[:person][:company_id]
   end
 
   def update
   end
 
   def destroy
+      Person.destroy(params[:id])
+      redirect_to "/companies/#{@person.company.id}/people"
   end
 
   private
