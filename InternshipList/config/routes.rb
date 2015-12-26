c2ed4702aa586b3db2f579f82127c9c3ed6267b2
Lauren Granger <lr.granger21@gmail.com>
2015-12-26 15:22:13 -0800

    resources :sessions, :only => [:new, :create]

--- InternshipList/config/routes.rb
+++ InternshipList/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   resources :users, :only => [:new, :create]
 
+  resources :sessions, :only => [:new, :create]
+
   resources :companies do
     resources :people
   end
