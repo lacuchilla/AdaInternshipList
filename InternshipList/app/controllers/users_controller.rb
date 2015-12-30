429dd9ecb63865da95bc8edc0e8c363a8c30e24a
Lauren Granger <lr.granger21@gmail.com>
2015-12-30 11:06:25 -0800

    create new user method with error flashes

--- InternshipList/app/controllers/users_controller.rb
+++ InternshipList/app/controllers/users_controller.rb
@@ -6,4 +6,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to companies_path
+    else
+      flash.now[:error] = "Please ensure you are using a valid email address and that your passwords match"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
