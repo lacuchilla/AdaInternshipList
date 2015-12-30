f957d346753b0858821aa84242ce929def2b06a7
Lauren Granger <lr.granger21@gmail.com>
2015-12-30 14:22:40 -0800

    successful new session creates welcome flash and redirects to companies show view

--- InternshipList/app/controllers/sessions_controller.rb
+++ InternshipList/app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    data = params[:session_data]
+    @user = User.find_by_email(data[:email])
+    if @user.nil?
+      flash.now[:error] = "That email and password do not match or do not exist. Please try again or sign up."
+      render :index
+    else
+      flash.now[:login] = "Welcome #{@user.first_name}!"
+      redirect_to companies_path
+    end
   end
 
   def destroy
+
   end
 end
