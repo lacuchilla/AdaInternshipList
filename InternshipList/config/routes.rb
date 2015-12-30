c9081a151d0a9fb76e03aa60cf3b733689aab98d
Lauren Granger <lr.granger21@gmail.com>
2015-12-30 14:00:29 -0800

    removed unneeded users index route

--- InternshipList/config/routes.rb
+++ InternshipList/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'sessions#index'
 
-  resources :users, :only => [:index,:new, :create]
+  resources :users, :only => [:new, :create]
 
   resources :sessions, :only => [:new, :create]
   delete "/logout", to: "sessions#destroy", as: :logout
